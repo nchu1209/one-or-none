15ad71bedf4479510d5569bc65f6fe4491f54bba
Nicole Chu <nchu1209@gmail.com>
2014-10-29 23:25:15 -0500

    Version 0.1

---
+++ HelloWorld.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class HelloWorld
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblMessage.Text = ""
+    End Sub
+
+    Protected Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        lblMessage.Text = "Hello World! :D"
+    End Sub
+End Class
