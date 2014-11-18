90c07111e15e43273ee923d08616f496b77b7d4e
Nicole Chu <nchu1209@gmail.com>
2014-11-18 14:57:37 -0600

    N - Customer Master; should show name when you log in. No date yet.

---
+++ CustomerMaster.Master.vb
@@ -0,0 +1,14 @@
+﻿Public Class CustomerMaster
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblName.Text = Session("CustomerFirstName").ToString
+        lblName2.Text = Session("CustomerFirstName").ToString
+    End Sub
+
+    Protected Sub lnkLogout_Click(sender As Object, e As EventArgs) Handles lnkLogout.Click
+        Session("CustomerNumber") = Nothing
+        Session("CustomerFirstName") = Nothing
+        Response.Redirect("CustomerLogin.aspx")
+    End Sub
+End Class
