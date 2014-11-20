0dd11e68500ef7b9d26fda755307f51ed386e8b5
Leah Carroll <leah.carroll@live.com>
2014-11-20 11:47:03 -0600

    Employee Stuff

--- EmployeeMaster.Master.designer.vb
+++ EmployeeMaster.Master.designer.vb
@@ -14,13 +14,13 @@ Option Explicit On
 Partial Public Class EmployeeMaster
 
     '''<summary>
-    '''form2 control.
+    '''form1 control.
     '''</summary>
     '''<remarks>
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents form2 As Global.System.Web.UI.HtmlControls.HtmlForm
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
     '''lblName control.
@@ -75,15 +75,6 @@ Partial Public Class EmployeeMaster
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents head As Global.System.Web.UI.WebControls.ContentPlaceHolder
-
-    '''<summary>
-    '''form1 control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
     '''ContentPlaceHolder1 control.
