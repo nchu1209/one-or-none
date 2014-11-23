936bc10bbbb8156f36f81ea23e81942d67545135
Nicole Chu <nchu1209@gmail.com>
2014-11-23 16:25:26 -0600

    N - pay bills changes

--- CustomerPayBills.aspx.vb
+++ CustomerPayBills.aspx.vb
@@ -42,6 +42,7 @@
     End Sub
 
     Protected Sub txtPay_Click(sender As Object, e As EventArgs) Handles btnPay.Click
+
         'validate selected account balance >= 0
 
 
@@ -66,20 +67,23 @@
                     Exit Sub
                 End If
             End If
-
         Next
 
         'build the two totals wooooo
         For i = 0 To gvMyPayees.Rows.Count - 1
-            Dim t As TextBox = DirectCast(gvMyPayees.Rows(i).Cells(4).FindControl("txtAmount"), TextBox)
+            Dim t As TextBox = DirectCast(gvMyPayees.Rows(i).Cells(3).FindControl("txtAmount"), TextBox)
+            Dim c As Calendar = DirectCast(gvMyPayees.Rows(i).Cells(4).FindControl("calDate"), Calendar)
 
-            If t.Text <> "" Then
+            If dbdate.CheckSelectedDate(c.SelectedDate) = 0 And t.Text <> "" Then
                 mdecTotalToday += CDec(t.Text)
             End If
-        Next
-        lblMessageTotal.Text = "Total Amount = " & mdecTotalToday.ToString("c2")
 
-        'if in future, schedule as pending payment
+            If dbdate.CheckSelectedDate(c.SelectedDate) = 1 And t.Text <> "" Then
+                mdecTotalPending += CDec(t.Text)
+            End If
+
+        Next
+        lblMessageTotal.Text = "Total Amount Today = " & mdecTotalToday.ToString("c2") & " and Total Amount Pending = " & mdecTotalPending.ToString("c2")
 
     End Sub
 
