20fb583260ede0aaba5b407d838bc1c331a4939c
amyenrione <amyenrione@utexas.edu>
2014-11-23 14:03:17 -0600

    no message

--- ClassDBAccounts.vb
+++ ClassDBAccounts.vb
@@ -398,6 +398,10 @@ Public Class ClassDBAccounts
         RunProcedureOneParameterTransfer2("usp_accounts_get_balance_by_account_number", "@AccountNumber", strAccountNumber)
     End Sub
 
+    Public Sub GetBalanceByAccountNumber2(strAccountNumber As String)
+        RunProcedureOneParameterTransfer("usp_accounts_get_balance_by_account_number", "@AccountNumber", strAccountNumber)
+    End Sub
+
     Public Sub GetCheckingandSavingsByCustomerNumber(strAccountNumber As String)
         RunProcedureOneParameter("usp_accounts_get_checking_and_savings_by_customer_number", "@AccountNumber", strAccountNumber)
     End Sub
