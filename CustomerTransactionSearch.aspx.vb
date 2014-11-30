9838742854be6aa4fc1bd00e9d27eaa9f3db4629
amyenrione <amyenrione@utexas.edu>
2014-11-30 14:48:04 -0600

    transaction search updates

--- CustomerTransactionSearch.aspx.vb
+++ CustomerTransactionSearch.aspx.vb
@@ -2,6 +2,7 @@
     Inherits System.Web.UI.Page
     Dim DBTransactions As New ClassDBTransactions
     Dim DBAccount As New ClassDBAccounts
+    Dim Val As New ClassValidate
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If IsPostBack = False Then
@@ -33,18 +34,16 @@
     Public Sub Clear()
         txtDescriptionSearch.Text = ""
         rblDescription.SelectedValue = Nothing
-        rblDescriptionAO.SelectedValue = Nothing
         ddlSearchByTransactionType.SelectedIndex = 0
-        rblTransactionTypeAO.SelectedValue = Nothing
         rblPrice.SelectedValue = Nothing
-        rblPriceAO.SelectedValue = Nothing
         txtSearchByOtherPriceMax.Text = ""
         txtSearchByOtherPriceMin.Text = ""
         txtSearchByTransactionNumber.Text = ""
-        rblTransactionNumberAO.SelectedValue = Nothing
         rblDate.SelectedValue = Nothing
-        rblDateAO.SelectedValue = Nothing
-        txtCustomDateSearch.Text = ""
+        txtCustomDateMin.Text = ""
+        For i As Integer = 0 To cblParameters.Items.Count - 1
+            cblParameters.Items(i).Selected = Nothing
+        Next
     End Sub
 
     Protected Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
@@ -62,68 +61,120 @@
         strTransactionTypeCode1 = "[Transaction Type] like '"
         strTransactionTypeCode2 = "'"
 
-        ' code - value - code
-        Dim strMin
-        If rblPrice.SelectedIndex = 0 Then
+        Dim str0to100 As String
+        Dim str101to200 As String
+        Dim str201to300 As String
+        Dim str300orMore As String
+        Dim strPriceCode1 As String
+        Dim strPriceCode2 As String
+        Dim strPriceCode3 As String
+        str0to100 = "Amount > 0 and Amount <=100"
+        str101to200 = "Amount > 100 and Amount <=200"
+        str201to300 = "Amount > 200 and amount <=300"
+        str300orMore = "Amount > 300"
+        strPriceCode1 = "Amount >= '"
+        strPriceCode2 = "' AND Amount <= '"
+        strPriceCode3 = "'"
 
-        End If
-        Dim str0to1001 As String
-        Dim str0to1002 As String
-        str0to1001 = "Amount >= 0 and"
-        str0to1002 = ""
+        Dim strTransactionNumberCode1 As String
+        Dim strTransactionNumberCode2 As String
+        strTransactionNumberCode1 = "[Transaction Number] like '%"
+        strTransactionNumberCode2 = "%'"
+
 
         Dim strIn1 As String
         Dim strIn2 As String
         Dim strIn3 As String
         Dim strIn4 As String
         Dim strIn5 As String
-        Dim strAndOr1 As String
-        Dim strAndOr2 As String
-        Dim strAndOr3 As String
-        Dim strAndOr4 As String
-        Dim strAndOr5 As String
 
-
-        If rblDescriptionAO.SelectedIndex = 0 Then
-            strAndOr1 = "And"
-        ElseIf rblDescriptionAO.SelectedIndex = 1 Then
-            strAndOr1 = "Or"
+        If cblParameters.Items(0).Selected = True Then
+            If txtDescriptionSearch.Text = "" Then
+                lblError.Text = "If you are searching by description, please enter a description into the Search by Description box"
+                Exit Sub
+            End If
+            If rblDescription.SelectedValue = Nothing Then
+                lblError.Text = "If you are searching by description, please select either a partial or a keyword search"
+                Exit Sub
+            End If
         End If
 
-        ''''can we do or?
-        ' and or or goes BEFORE info
-        'ie I want to search by "or date"
-        'therefore strandor4 must go before date
-        'how do we handle the first one 
-
-        If rblDescriptionAO.SelectedIndex = 0 And rblDescription.SelectedIndex = 0 Then
-            strAndOr1 = "And"
+        If cblParameters.Items(0).Selected = True And rblDescription.SelectedIndex = 0 Then
             strIn1 = strDescriptionPartialCode1 & txtDescriptionSearch.Text & strDescriptionPartialCode2
-        ElseIf rblDescriptionAO.SelectedIndex = 0 And rblDescription.SelectedIndex = 1 Then
-            strAndOr1 = "And"
-            strIn1 = strDescriptionKeywordCode1 & txtDescriptionSearch.Text & strDescriptionKeywordCode2
-        ElseIf rblDescriptionAO.SelectedIndex = 1 And rblDescription.SelectedIndex = 0 Then
-            strAndOr1 = "Or"
-            strIn1 = strDescriptionPartialCode1 & txtDescriptionSearch.Text & strDescriptionPartialCode2
-        ElseIf rblDescriptionAO.SelectedIndex = 1 And rblDescription.SelectedIndex = 1 Then
-            strAndOr1 = "Or"
-            strIn1 = strDescriptionKeywordCode1 & txtDescriptionSearch.Text & strDescriptionKeywordCode2
-        Else
-            st()
         End If
 
-        If rblTransactionTypeAO.SelectedIndex = 0 Then
-            strAndOr2 = "And"
+        If cblParameters.Items(0).Selected = True And rblDescription.SelectedIndex = 1 Then
+            strIn1 = strDescriptionKeywordCode1 & txtDescriptionSearch.Text & strDescriptionKeywordCode2
         End If
+
+
+        If cblParameters.Items(1).Selected = True Then
+            strIn2 = strTransactionTypeCode1 & ddlSearchByTransactionType.SelectedValue.ToString & strTransactionTypeCode2
+        End If
+
+        If cblParameters.Items(2).Selected = True Then
+            If rblPrice.SelectedIndex = Nothing Then
+                lblError.Text = "If you want to search by price, please select a price range to search by"
+                Exit Sub
+            End If
+            If rblPrice.SelectedValue = "Custom Range" Then
+                If txtSearchByOtherPriceMax.Text = "" Or txtSearchByOtherPriceMin.Text = "" Then
+                    lblError.Text = "Please enter a minimum and a maximum transaction amount"
+                    Exit Sub
+                End If
+                If Val.CheckDecimal(txtSearchByOtherPriceMax.Text) = -1 Then
+                    lblError.Text = "Please enter a positive numeric amount as your maximum price"
+                    Exit Sub
+                End If
+                If Val.CheckDecimal(txtSearchByOtherPriceMin.Text) = -1 Then
+                    lblError.Text = "Please enter a positive numeric amount as your minimum price"
+                    Exit Sub
+                End If
+            End If
+        End If
+
+        If cblParameters.Items(2).Selected = True Then
+            If rblPrice.SelectedIndex = 0 Then
+                strIn3 = str0to100
+            End If
+            If rblPrice.SelectedIndex = 1 Then
+                strIn3 = str101to200
+            End If
+            If rblPrice.SelectedIndex = 2 Then
+                strIn3 = str201to300
+            End If
+            If rblPrice.SelectedIndex = 3 Then
+                strIn3 = str300orMore
+            End If
+            If rblPrice.SelectedIndex = 4 Then
+                strIn3 = strPriceCode1 & txtSearchByOtherPriceMin.Text & strPriceCode2 & txtSearchByOtherPriceMax.Text & strPriceCode3
+            End If
+        End If
+
+        If cblParameters.Items(3).Selected = True Then
+            If txtSearchByTransactionNumber.Text = "" Then
+                lblError.Text = "Please enter a transaction number to search by if you want to search by transaction number"
+                Exit Sub
+            End If
+            If Val.CheckDigits(txtSearchByTransactionNumber.Text) = False Then
+                lblError.Text = "Please enter a numeric transaction number to search"
+                Exit Sub
+            End If
+        End If
+        If cblParameters.Items(3).Selected = True Then
+            strIn4 = strTransactionNumberCode1 & txtSearchByTransactionNumber.Text & strTransactionNumberCode2
+        End If
+
+        'systemdate vs date in row
 
         DBTransactions.Search2Param(strDescriptionKeywordCode1, txtDescriptionSearch.Text, strDescriptionKeywordCode2, "And", strTransactionTypeCode1, ddlSearchByTransactionType.SelectedValue.ToString, strTransactionTypeCode2)
 
         Search()
-        lblNumberOfTransactions.Text = rblDescriptionAO.SelectedValue.ToString
         Clear()
     End Sub
 
     Protected Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         Clear()
+        'reload gv to original
     End Sub
 End Class
