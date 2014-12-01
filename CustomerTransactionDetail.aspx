00eade333c4b1691248609a383a1a0559c823919
caking789 <caking789@gmail.com>
2014-11-30 20:44:01 -0600

    Customer Trans. Detail

---
+++ CustomerTransactionDetail.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/CustomerMaster.Master" CodeBehind="CustomerTransactionDetail.aspx.vb" Inherits="KProject.CustomerTransactionDetail" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <link href="ContentStyle.css" rel="stylesheet" type="text/css" />
+    <div id ="title">
+        Transaction Details<br />
+        <br />
+    </div>
+    <div id ="center">
+       <div id ="subtitle">Account Details</div>
+       <div id ="label2">
+           <asp:Label ID="lblCustomerID" runat="server" Text="Description:"></asp:Label>
+           <br />
+           <asp:Label ID="Label1" runat="server" Text="Transaction Type:"></asp:Label>
+           <br />
+           <asp:Label ID="Label10" runat="server" Text="Amount:"></asp:Label>
+           <br />
+           <asp:Label ID="Label11" runat="server" Text="Transaction Number:"></asp:Label>
+           <br />
+           
+        </div>
+        <div id ="textbox">
+            <asp:TextBox ID="txtDescription" runat="server" ReadOnly="True"></asp:TextBox>
+            <br />
+             <asp:TextBox ID="txtTransactionType" runat="server" ReadOnly="True"></asp:TextBox>
+            <br />
+             <asp:TextBox ID="txtAmount" runat="server" ReadOnly="True"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtTransactionNumber" runat="server" ReadOnly="True"></asp:TextBox>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+       
+    </div>
+</asp:Content>
---
+++ CustomerTransactionDetail.aspx.vb
@@ -0,0 +1,51 @@
+﻿Option Strict On
+
+Public Class CustomerTransactionDetail
+    Inherits System.Web.UI.Page
+
+    Dim DBTransactions As New ClassDBTransactions
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Session("CustomerNumber") Is Nothing Then
+            Response.Redirect("CustomerLogin.aspx")
+
+            'lblTransactionID=Session("
+
+            'DBTransactions.GetAccountByCustomerNumber(Session("CustomerNumber").ToString)
+
+            ''get the record id from the select
+            'mCustomerID = CInt(Session("CustomerNumber"))
+
+            If IsPostBack = False Then
+                'ReloadDatasetAndDDL()
+                FillTextboxes()
+            End If
+
+        End If
+    End Sub
+
+    Private Sub FillTextboxes()
+
+        'declare variables
+        'Dim strPhone As String
+
+        'populate variables
+        'strPhone = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("phone").ToString
+
+
+        'fill textboxes
+        'txtDescription.Text = DBTransactions.CustDataset.Tables("tblCustomers").Rows(0).Item("lastname").ToString
+        'txtTransactionType.Text = DBTransactions.CustDataset.Tables("tblCustomers").Rows(0).Item("firstname").ToString
+        'txtAmount.Text = DBTransactions.CustDataset.Tables("tblCustomers").Rows(0).Item("MI").ToString
+        'txtTransactionNumber.Text = DBTransactions.CustDataset.Tables("tblCustomers").Rows(0).Item("password").ToString
+        'txtAddress.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("address").ToString
+        'txtCity.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("city").ToString
+        'txtState.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("state").ToString
+        'txtZip.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("zipcode").ToString
+        'txtEmail.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("emailaddr").ToString
+        'txtPhone.Text = DB.CustDataset.Tables("tblCustomers").Rows(0).Item("phone").ToString
+
+    End Sub
+
+
+End Class
